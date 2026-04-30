9ced4505060de936627bc1be57e99c18bd1f6890
Dmitry Kurochkin <kudima03@gmail.com>
2026-04-30 13:58:31 +0300

    FakeTests implemented

---
+++ src/Tests/Pure.RelationalSchema.RichRelationalModel.EFCore.Models.Configurations.Tests/FakeTests.cs
@@ -0,0 +1,9 @@
+using Xunit;
+
+namespace Pure.RelationalSchema.RichRelationalModel.EFCore.Models.Configurations.Tests;
+
+public sealed record FakeTests
+{
+    [Fact]
+    public void FakeTest() { }
+}
